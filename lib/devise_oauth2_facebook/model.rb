94c7feb9ed05c749c7209ff6ca81b730fa8a6708
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2010-06-11 16:08:45 +0100

    should be working completely

--- lib/devise_oauth2_facebook/model.rb
+++ lib/devise_oauth2_facebook/model.rb
@@ -5,18 +5,11 @@ module Devise
       extend ActiveSupport::Concern
 
       def do_update_facebook_user(fb_user, token)
-        # Rails.logger.info "id: #{fb_user.id.inspect}"
-        # Rails.logger.info "email: #{fb_user.email.inspect}"
-        # Rails.logger.info "name: #{fb_user.name.inspect}"
         self.send("#{self.class.facebook_uid_field}=".to_sym, fb_user.id)
         self.send("#{self.class.facebook_token_field}=".to_sym, token)
-        self.email = fb_user.email.downcase
+        self.email = fb_user.email.to_s.downcase if fb_user.respond_to?(:email)
         update_facebook_user(fb_user)
-        if self.respond_to?(:remember_me!)
-          self.remember_me!
-        else
-          self.save_without_validation
-        end
+        self.save_without_validation
       end
       
       def update_facebook_user(fb_user)
@@ -33,7 +26,6 @@ module Devise
         Devise::Models.config(self, :facebook_uid_field, :facebook_token_field)
 
         def find_with_facebook_user(fb_user, token)
-          # Rails.logger.info "TEST1: facebook_uid_field => fb_user.id, #{facebook_uid_field.inspect} => #{fb_user.id}"
           user = where(facebook_uid_field.to_sym => fb_user.id).first || where(:email => fb_user.email.downcase).first
           if user
             user.do_update_facebook_user(fb_user, token)
@@ -42,8 +34,7 @@ module Devise
         end
         
         def create_with_facebook_user(fb_user, token)
-          # Rails.logger.info "TEST2: facebook_uid_field => fb_user.id, #{facebook_uid_field.inspect} => #{fb_user.id}"
-          user = new(facebook_uid_field.to_sym => fb_user.id)
+          user = new(facebook_uid_field.to_sym => fb_user.id, :password => "fakepass", :password_confirmation => "fakepass")
           user.skip_confirmation! if user.respond_to?(:skip_confirmation!)
           user.do_update_facebook_user(fb_user, token)
           user
