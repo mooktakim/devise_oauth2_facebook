363df5cde6ee1995efe364468e5c7b2d8cd3ec9d
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2010-07-24 15:55:59 +0100

    respond_to? always returns nil

--- lib/devise_oauth2_facebook/model.rb
+++ lib/devise_oauth2_facebook/model.rb
@@ -7,7 +7,7 @@ module Devise
       def do_update_facebook_user(fb_user, token)
         self.send("#{self.class.facebook_uid_field}=".to_sym, fb_user.id)
         self.send("#{self.class.facebook_token_field}=".to_sym, token)
-        self.email = fb_user.email.to_s.downcase if fb_user.respond_to?(:email)
+        self.email = fb_user.email.to_s.downcase if fb_user.email.present?
         update_facebook_user(fb_user)
         self.save(:validate => false)
       end
