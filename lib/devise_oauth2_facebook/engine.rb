788863e59fd56f7092b63246be679812382dcf4c
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2010-08-04 16:22:03 +0100

    rails 3 rc does not auto load lib anymore

--- lib/devise_oauth2_facebook/engine.rb
+++ lib/devise_oauth2_facebook/engine.rb
@@ -3,5 +3,8 @@ require "devise_oauth2_facebook/routes"
 module DeviseOauth2Facebook
   class Engine < ::Rails::Engine
     config.devise_oauth2_facebook = DeviseOauth2Facebook
+    
+    config.autoload_paths << File.expand_path(File.join(File.dirname(__FILE__), ".."))
+    
   end
 end
