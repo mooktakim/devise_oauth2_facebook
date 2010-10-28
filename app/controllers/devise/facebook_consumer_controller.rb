7a0d55113a1cd9e94b34a7d1cc9b89a16b00dd7e
Nina Jansen <info@ninajansen.dk>
2010-10-28 17:37:20 +0200

    new version that allows the rails application to specify additional permissions and the url that the application has to redirect to after authorization

--- app/controllers/devise/facebook_consumer_controller.rb
+++ app/controllers/devise/facebook_consumer_controller.rb
@@ -8,8 +8,15 @@ class Devise::FacebookConsumerController < ApplicationController
     else
       scope = Devise.facebook_permissions
     end
-
     url = send("#{resource_name}_fb_callback_url".to_sym)
+    if params[:permission] && params[:go_back]
+      url = url + "?permission=#{params[:permission]}&go_back=#{params[:go_back]}"
+    elsif params[:permission]
+       url = url + "?permission=#{params[:permission]}"
+    elsif params[:go_back]
+       url = url + "?go_back=#{params[:go_back]}"
+    end
+    
     uri = facebook_client.authorization.authorize_url(:redirect_uri => url, :scope => scope)
     if Devise.facebook_canvas_app
       render :layout => false, :inline => "<script type='text/javascript' charset='utf-8'>top.location.href='#{uri}';</script>"
@@ -20,16 +27,32 @@ class Devise::FacebookConsumerController < ApplicationController
   
   def callback
     url = send("#{resource_name}_fb_callback_url".to_sym)
-
+    if params[:permission] && params[:go_back]
+      url = url + "?permission=#{params[:permission]}&go_back=#{CGI::escape(params[:go_back])}"
+    elsif params[:permission]
+      url = url + "?permission=#{params[:permission]}"
+    elsif params[:go_back]
+      url = url + "?go_back=#{CGI::escape(params[:go_back])}"
+    end
+      
     client = facebook_client
     client.authorization.process_callback(params[:code], :redirect_uri => url)
-
+    
     token = client.access_token
+    
     fb_user = client.selection.me.info!
-
-    resource = resource_class.find_with_facebook_user(fb_user, token, client)
+    
+    if !!params[:permission]
+      permissions = Devise.facebook_permissions + ",#{params[:permission]}"
+    else
+      permissions = Devise.facebook_permissions
+    end
+      
+    options = {:permissions => permissions}
+    
+    resource = resource_class.find_with_facebook_user(fb_user, token, client, options)
     unless resource
-      resource = resource_class.create_with_facebook_user(fb_user, token, client)
+      resource = resource_class.create_with_facebook_user(fb_user, token, client, options)
     end
     if resource_class.respond_to?(:serialize_into_cookie)
       resource.remember_me!
@@ -43,7 +66,11 @@ class Devise::FacebookConsumerController < ApplicationController
     
     if Devise.facebook_canvas_app
       sign_in(resource_name, resource)
-      redirect_to Devise.facebook_canvas_url
+      if params[:go_back]
+        redirect_to Devise.facebook_canvas_url + params[:go_back]
+      else
+        redirect_to Devise.facebook_canvas_url
+      end
     else
       sign_in_and_redirect(resource_name, resource)
     end
--- lib/devise_oauth2_canvas_facebook/model.rb
+++ lib/devise_oauth2_canvas_facebook/model.rb
@@ -4,11 +4,11 @@ module Devise
     module DeviseOauth2CanvasFacebook
       extend ActiveSupport::Concern
 
-      def do_update_facebook_user(fb_user, token, client)
+      def do_update_facebook_user(fb_user, token, client, options)
         self.send("#{self.class.facebook_uid_field}=".to_sym, fb_user.id)
         self.send("#{self.class.facebook_token_field}=".to_sym, token)
         self.email = fb_user.email.to_s.downcase if fb_user.email.present?
-        update_facebook_user(fb_user, client)
+        update_facebook_user(fb_user, client, options)
         self.save(:validate => false)
       end
       
@@ -25,18 +25,18 @@ module Devise
       module ClassMethods
         Devise::Models.config(self, :facebook_uid_field, :facebook_token_field)
 
-        def find_with_facebook_user(fb_user, token,client)
+        def find_with_facebook_user(fb_user, token,client, options)
           user = where(facebook_uid_field.to_sym => fb_user.id).first
           if user
-            user.do_update_facebook_user(fb_user, token, client)
+            user.do_update_facebook_user(fb_user, token, client,options)
           end
           user
         end
         
-        def create_with_facebook_user(fb_user, token,client)
+        def create_with_facebook_user(fb_user, token,client,  options)
           user = new(facebook_uid_field.to_sym => fb_user.id, :password => "fakepass", :password_confirmation => "fakepass")
           user.skip_confirmation! if user.respond_to?(:skip_confirmation!)
-          user.do_update_facebook_user(fb_user, token, client)
+          user.do_update_facebook_user(fb_user, token, client, options)
           user
         end
       end
