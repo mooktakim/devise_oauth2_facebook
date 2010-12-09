0f866699dfbc17ef50f7fd0897fe77c017fb493a
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2010-12-09 13:16:53 +0000

    oops bug

--- lib/devise_oauth2_facebook/facebook_consumer_helper.rb
+++ lib/devise_oauth2_facebook/facebook_consumer_helper.rb
@@ -2,9 +2,9 @@ module DeviseOauth2Facebook::FacebookConsumerHelper
   
   def facebook_client(token = nil)
     if token.present?
-      FBGraph::Client.new(:client_id => Devise.facebook_api_key, :secret_id => Devise.facebook_api_secret)
-    else
       FBGraph::Client.new(:client_id => Devise.facebook_api_key, :secret_id => Devise.facebook_api_secret, :token => token)
+    else  
+      FBGraph::Client.new(:client_id => Devise.facebook_api_key, :secret_id => Devise.facebook_api_secret)
     end
   end
   
