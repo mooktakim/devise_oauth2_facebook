d3310019a0f854335dbc3468e4dfefa5abc4e408
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2011-02-12 19:57:48 +0000

    fb id fix found bu flobby

--- lib/devise_oauth2_facebook/model.rb
+++ lib/devise_oauth2_facebook/model.rb
@@ -5,7 +5,7 @@ module Devise
       extend ActiveSupport::Concern
 
       def do_update_facebook_user(fb_user, token)
-        self.send("#{self.class.facebook_uid_field}=".to_sym, fb_user.id)
+        self.send("#{self.class.facebook_uid_field}=".to_sym, fb_user["id"])
         self.send("#{self.class.facebook_token_field}=".to_sym, token)
         self.email = fb_user.email.to_s.downcase if fb_user.email.present?
         update_facebook_user(fb_user)
@@ -26,7 +26,7 @@ module Devise
         Devise::Models.config(self, :facebook_uid_field, :facebook_token_field)
 
         def find_with_facebook_user(fb_user, token)
-          user = where(facebook_uid_field.to_sym => fb_user.id).first || where(:email => fb_user.email.downcase).first
+          user = where(facebook_uid_field.to_sym => fb_user["id"]).first || where(:email => fb_user.email.downcase).first
           if user
             user.do_update_facebook_user(fb_user, token)
           end
@@ -34,7 +34,7 @@ module Devise
         end
         
         def create_with_facebook_user(fb_user, token)
-          user = new(facebook_uid_field.to_sym => fb_user.id, :password => "fakepass", :password_confirmation => "fakepass")
+          user = new(facebook_uid_field.to_sym => fb_user["id"], :password => "fakepass", :password_confirmation => "fakepass")
           user.skip_confirmation! if user.respond_to?(:skip_confirmation!)
           user.do_update_facebook_user(fb_user, token)
           user
