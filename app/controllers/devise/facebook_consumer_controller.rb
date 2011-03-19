930b29f3b8cc9d86f07ce6c9ca5dfe44ee54f66f
Joachim Filip Ignacy Bartosik <jbartosik@gmail.com>
2011-03-19 13:34:12 +0100

    Make Devise::FacebookConsumerController#callback an action method
    For some reason Devise::FacebookConsumerController#callback isn't
    recognized as action method, so we need to fix this.

--- app/controllers/devise/facebook_consumer_controller.rb
+++ app/controllers/devise/facebook_consumer_controller.rb
@@ -2,4 +2,9 @@ class Devise::FacebookConsumerController < ApplicationController
   include Devise::Controllers::InternalHelpers
   include DeviseOauth2Facebook::FacebookConsumerHelper
   include DeviseOauth2Facebook::ControllerMethods
+
+  def initialize
+    # For some reason callback is not recognized as action_method automagically
+    action_methods.add "callback"
+  end
 end
